cc9ee1e31b233c0b8f35cfc4785f0e8572b278c8
rwilliaise <rwilliaise@users.noreply.github.com>
2022-04-21 18:48:13 -0700

    Add basic generator files

--- Generator/Generator.h
+++ Generator/Generator.h
@@ -1,11 +1,16 @@
 #pragma once
+
+#include "NoiseGenerator.h"
+
 class Generator
 {
 public:
+	explicit Generator(NoiseGenerator generator): generator(generator) {}
 
+	void generate();
+	void applyVertices();
 
 private:
-
-	
+	NoiseGenerator generator;
 };
 
---
+++ Generator/NoiseGenerator.h
@@ -0,0 +1,7 @@
+#pragma once
+class NoiseGenerator
+{
+public: 
+	virtual float GetNoise(float, float);
+};
+
