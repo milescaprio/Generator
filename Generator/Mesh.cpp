f13ae56999bc2e9f758a49582a10dbb7c73dc936
PunkyMunky64 <46407361+PunkyMunky64@users.noreply.github.com>
2022-05-19 20:37:26 -0700

    V1.1.0
    Removed print statements which took like 29 million extra hours to run

--- Generator/Mesh.cpp
+++ Generator/Mesh.cpp
@@ -18,10 +18,10 @@ void Mesh::rotate(float midx, float midy, float ang) {
 		float r = VirtualTurtle::dist(midx, midy, p.x, p.y);
 		float theta = VirtualTurtle::ucAngle(midx, midy, p.x, p.y);
 		theta += ang;
-		std::cout << p.x << " " << p.y << " " << r << " " << theta << " ";
+		//std::cout << p.x << " " << p.y << " " << r << " " << theta << " ";
 		p.x = midx + r * cos(theta * PI / 180);
 		p.y = midy + r * sin(theta * PI / 180);
-		std::cout << p.x << " " << p.y << "\n";
+		//std::cout << p.x << " " << p.y << "\n";
 	}
 }
 
